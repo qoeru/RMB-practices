b088cb8baf766b0e0cd2818c9b84c73b8b83e116
Qoeru <ekaterinaakubson@gmail.com>
2023-02-11 14:01:05 +0300

    added toast to every step of MainActivity cycle

--- 1/app/src/main/java/com/example/practice/MainActivity.java
+++ 1/app/src/main/java/com/example/practice/MainActivity.java
@@ -8,40 +8,53 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    private void sendToast(){
         Context context = getApplicationContext();
         CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendToast();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        sendToast();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        sendToast();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        sendToast();
     }
     @Override
     protected void onPause() {
         super.onPause();
+
+        sendToast();
     }
     @Override
     protected void onResume() {
         super.onResume();
+
+        sendToast();
     }
 
 }
